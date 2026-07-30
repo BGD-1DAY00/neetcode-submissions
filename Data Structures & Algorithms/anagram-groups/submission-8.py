e41652a061606ebcb569e4c89ddded30cb33cbfd
Arc <91921824+BGD-1DAY00@users.noreply.github.com>
2026-07-30 10:34:05 -0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        m = {}
+
+        for idx, val in enumerate(strs):
+            l = [0] * 26
+            for j in val:
+                word_val = ord(j) - ord('a')
+                l[word_val] += 1
+            word_str = tuple(l)
+            if word_str in m: 
+                word_list = m[word_str]
+                word_list.append(val)
+            else:
+                m[word_str] = [val]
+        response = []
+        for key, val in m.items():
+            response.append(val)
+        return response
+
+        
